d2d190f1a39050a33fa471b8259bf09ea7a71877
Hannes Fostie <hannes.fostie@gmail.com>
2014-10-16 13:42:06 +0200

    Adds Suite#all and Suite#scrape_suites methods

--- lib/bisu/suite.rb
+++ lib/bisu/suite.rb
@@ -1,4 +1,30 @@
+require 'open-uri'
+require 'oga'
+
 module Bisu
   class Suite
+    RELEASES_PAGE = 'http://secure-testing.debian.net/debian-secure-testing/project/debsecan/release/1/'
+    NON_SUITE_LINK_HREFS = %w(
+      ?C=N;O=D
+      ?C=M;O=A
+      ?C=S;O=A
+      ?C=D;O=A
+      /debian-secure-testing/project/debsecan/release/
+      GENERIC
+    )
+
+    class << self
+      def all
+        scrape_suites
+      end
+    end
+
+  private
+    class << self
+      def scrape_suites
+        releases_page = Oga.parse_html(open(RELEASES_PAGE))
+        releases_page.xpath('//a/@href').map(&:text).reject { |text| NON_SUITE_LINK_HREFS.include?(text) }
+      end
+    end
   end
 end
