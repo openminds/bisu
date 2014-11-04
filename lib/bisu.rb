19adace25d70606d26d564145078747709a86c53
Hannes Fostie <hannes.fostie@gmail.com>
2014-11-04 10:12:02 +0100

    Renames Bisu::VulnerabiliyParser to Bisu::VulnerabilityParser::Debian

--- lib/bisu.rb
+++ lib/bisu.rb
@@ -2,7 +2,7 @@ require_relative 'bisu/version'
 
 require_relative 'bisu/platform'
 require_relative 'bisu/package'
-require_relative 'bisu/vulnerability_parser'
+require_relative 'bisu/vulnerability_parser/debian'
 
 module Bisu
 end
